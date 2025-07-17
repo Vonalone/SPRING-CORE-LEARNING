bba7136fbd0363487e01dcbe468cb6dadd2b3f9d
Hicham Hared <hichamharedsm@gmail.com>
2025-07-17 06:41:41 +0100

    chore: initialize project structure

---
+++ src/main/java/com/platform/spring_core_learning/SpringCoreLearningApplication.java
@@ -0,0 +1,13 @@
+package com.platform.spring_core_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/spring_core_learning/SpringCoreLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.spring_core_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
