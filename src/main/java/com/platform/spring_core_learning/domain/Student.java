4ac929c01c0b32dbaa3c4db480ff3d398c2a2bd9
Hicham Hared <hichamharedsm@gmail.com>
2025-07-17 08:06:00 +0100

    feat: add Student domain

---
+++ src/main/java/com/platform/spring_core_learning/domain/Student.java
@@ -0,0 +1,50 @@
+package com.platform.spring_core_learning.domain;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String grade ;
+
+
+    public Student (){
+
+    }
+    public Student(Long id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
