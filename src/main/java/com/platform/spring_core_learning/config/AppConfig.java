74fc7c7a5083206828e749031d7383c1c2943c35
Hicham Hared <hichamharedsm@gmail.com>
2025-07-24 20:09:07 +0100

    feat: add configuration class to scan components and initialize beans

---
+++ src/main/java/com/platform/spring_core_learning/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.platform.spring_core_learning.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.platform")
+public class AppConfig {
+
+}
